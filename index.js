057f6bdbda39827fbf952591019bcbbe7c1965f1
unknown <pantho883@gmail.com>
2025-11-23 01:30:05 +0600

    db connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 const route = require('./src/routes/routes')
+const mongoose = require('mongoose')
 const app = express()
 const port = 8000
 // dot env config
@@ -12,6 +13,13 @@ app.use(express.json())
 app.use(cors())
 // connect middleware route
 app.use(route)
+
+
+// db connection
+mongoose.connect(process.env.db_Link)
+.then(()=>{console.log(`db connected`)})
+.catch((err)=>{console.log(err)})
+
 
 
 
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 // libaries
 
+
 // registration controller 
 const register_Controller = async (req,res)=>{
     try{
@@ -20,4 +21,29 @@ const register_Controller = async (req,res)=>{
     }
 }
 
-module.exports = {register_Controller}
+
+// login controller 
+const login_Controller = async (req,res)=>{
+    try{
+        // get data from frontend
+        //   const {
+        //      userName,
+        //      email,
+        //      password,
+        //      phone,
+        //      address
+        //   } = req.body
+
+
+        // all ok
+        res.status(200).send(`Registered Successfully`)
+    }catch(err){
+      res.status(500).send(`${err}`)
+    }
+}
+
+// all exports
+module.exports = {
+    register_Controller,
+    login_Controller
+}
