54c1557e893f2508ec288bdf7ce0f3387b430caf
unknown <pantho883@gmail.com>
2025-12-14 20:42:09 +0600

    cupon controller updated done

--- src/controllers/cuponController.js
+++ src/controllers/cuponController.js
@@ -1,13 +1,13 @@
 const cuponModel = require("../models/cuponModel")
 
-
+// add-cupon -------------------------------------
 const cuponController = async (req,res)=>{
   try{
          const {cuponName,discountPrice} = req.body
 
          const existingCupon = await cuponModel.findOne({cuponName})
          
-         if(!existingCupon) return res.status(404).json({errorMessage:`${cuponName} already exists`})
+         if(existingCupon) return res.status(404).json({errorMessage:`${cuponName} does not exist`})
 
          await new cuponModel({cuponName,discountPrice}).save()
     // all ok 
@@ -18,7 +18,31 @@ const cuponController = async (req,res)=>{
   }
 }
 
+// update-cupon -------------------------------------
+const cuponUpdate_controller = async (req,res)=>{
+    try{
+
+        const {cuponId,cuponName,discountPrice} = req.body
+
+        const existingCupon = await cuponModel.findOne({_id:cuponId})
+
+        if(cuponName) existingCupon.cuponName = cuponName
+
+        if(discountPrice) existingCupon.discountPrice = discountPrice
+
+        await existingCupon.save()
+
+      // all ok 
+    res.status(201).json({data:`cupon updated`})
+
+    }catch(err){
+    console.log(err)
+    res.status(500).json({errorMessage:`Internal server error :${err}`})
+  }
+}
+
 
 module.exports = {
     cuponController,
-}
+    cuponUpdate_controller,
+} 
--- src/routes/api/cuponApi.js
+++ src/routes/api/cuponApi.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { cuponController } = require('../../controllers/cuponController')
+const { cuponController, cuponUpdate_controller } = require('../../controllers/cuponController')
 const cuponApi = express.Router()
 
 cuponApi.post('/cuponcreate',cuponController)
+cuponApi.post('/cuponupdate',cuponUpdate_controller)
 
 
 module.exports = cuponApi
